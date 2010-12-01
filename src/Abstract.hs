1d12b47026cd2be8a73681ca68c618f9a0ade562
Andreas Abel <andreas.abel@ifi.lmu.de>
2010-12-01 10:35:46 +0000

    started translation from C to A

---
+++ src/Abstract.hs
@@ -0,0 +1,26 @@
+module Abstract where
+
+import Concrete (Fixity)
+
+-- | Names in the abstract syntax are unique identifiers
+type Name = Int  -- Integer not deemed necessary
+
+newtype Declarations = Declarations { declarations :: [Declaration] }
+
+data Declaration
+  = TypeSig Name Type
+  | Defn Name (Maybe Type) Expr
+
+type Type = Expr
+data Expr
+  = Atom  Atom
+  | Pi    (Maybe Name) Type Type  -- ^ A -> B or {x:A} B
+  | Lam   Name (Maybe Type) Expr  -- ^ [x:A] E or [x]E
+  | App   Expr Expr               -- ^ E1 E2 
+
+data Atom 
+  = Var Name                      -- ^ locally bound identifier
+  | Con Name Fixity               -- ^ declared constant
+  | Def Name Fixity               -- ^ defined identifier
+  | Typ                           -- ^ type
+
