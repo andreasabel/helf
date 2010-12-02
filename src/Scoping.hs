a22fa976b6bb1905f33524b018300a5316f2413d
Andreas Abel <andreas.abel@ifi.lmu.de>
2010-12-02 17:24:07 +0000

    bla

--- src/Scoping.hs
+++ src/Scoping.hs
@@ -11,6 +11,8 @@ import qualified Concrete as C
 import qualified Abstract as A
 import qualified OperatorPrecedenceParser as O
 
+-- * abstract scoping monad
+
 class (Applicative m, Monad m) => Scope m where
   addCon    :: C.Name -> m A.Name
   addDef    :: C.Name -> m A.Name 
@@ -22,17 +24,32 @@ class (Applicative m, Monad m) => Scope m where
   getAtom n  = fst <$> getAtomAndFixity n
   parseError :: O.ParseError -> m a
 
+-- * implementation of scoping monad by state/error
+
+{- Scoping need the following components
+
+- a map from C.Name to A.Name
+- a map from A.Name to (A.Atom, C.Fixity)
+
+  this resolves the parsed concrete names into unique identifiers
+  with their fixity 
+
+  My view: A fixity can only be set before the name is first used.
+  Twelf  : A fixity can be set and reset at any time.
+
+- a map from A.Name to 
+
+-}
+data ScopeState = ScopeState
+  { 
+  }
+
+-- * parsing
 class Parse c a where 
   parse :: Scope m => c -> m a
 
-class Print c a where
-  print :: Scope m => a -> m c 
-
 instance Parse c a => Parse (Maybe c) (Maybe a) where
   parse = mapM parse
-
-instance Print c a => Print (Maybe c) (Maybe a) where
-  print = mapM print
 
 instance Parse C.Declaration [A.Declaration] where
   parse cdecl = 
@@ -41,19 +58,29 @@ instance Parse C.Declaration [A.Declaration] where
       C.Defn n mt e -> return <$> (A.Defn <$> addDef n <*> parse mt <*> parse e)
       C.Fixity n fx -> const [] <$> addFixity n fx
 
-instance Print C.Declaration A.Declaration where
-  print adecl =
-    case adecl of
-      A.TypeSig n t -> C.TypeSig <$> getName n <*> print t
-      A.Defn n mt e -> C.Defn <$> getName n <*> print mt <*> print e
-
-type Item = O.Item Int A.Expr
-
 instance Parse C.Atom A.Atom where
   parse a = 
     case a of
       C.Typ     -> return $ A.Typ
       C.Ident n -> getAtom n
+
+instance Parse C.Expr A.Expr where
+  parse cexpr =
+    case cexpr of
+      C.Atom a     -> A.Atom <$> parse a
+      C.Fun t1 t2  -> A.Pi Nothing <$> parse t1 <*> parse t2
+      C.Pi x t1 t2 -> do
+        t1 <- parse t1
+        addVar x $ \ x -> A.Pi (Just x) t1 <$> parse t2
+      C.Lam x mt e -> do
+        mt <- parse mt
+        addVar x $ \ x -> A.Lam x mt <$> parse e
+      C.Apps es -> parseApplication =<< mapM parse es
+
+-- applications [C.Expr] are parsed into list of Stack items 
+-- which is then resolved into an A.Expr
+
+type Item = O.Item Int A.Expr
 
 instance Parse C.Atom Item where
   parse catom = 
@@ -71,30 +98,6 @@ instance Parse C.Expr Item where
       C.Atom a -> parse a
       _        -> O.Atom <$> parse cexpr
 
-instance Parse C.Expr A.Expr where
-  parse cexpr =
-    case cexpr of
-      C.Atom a     -> A.Atom <$> parse a
-      C.Fun t1 t2  -> A.Pi Nothing <$> parse t1 <*> parse t2
-      C.Pi x t1 t2 -> do
-        t1 <- parse t1
-        addVar x $ \ x -> A.Pi (Just x) t1 <$> parse t2
-      C.Lam x mt e -> do
-        mt <- parse mt
-        addVar x $ \ x -> A.Lam x mt <$> parse e
-      C.Apps es -> parseApplication =<< mapM parse es
-
-instance Print C.Expr A.Expr where
-  print aexpr = 
-    case aexpr of
-      A.Atom a            -> C.Atom <$> print a
-      A.Pi Nothing  t1 t2 -> C.Fun <$> print t1 <*> print t2
-      A.Pi (Just x) t1 t2 -> C.Pi <$> getName x <*> print t1 <*> print t2
-      A.Lam x mt e        -> C.Lam <$> getName x <*> print mt <*> print e
-      A.App{}             -> C.Apps <$> mapM print (printApplication aexpr)
-
-instance Print C.Atom A.Atom where
-
 instance O.Juxtaposition A.Expr where
   juxtaposition = A.App
 
@@ -104,5 +107,30 @@ parseApplication is =
     Left err -> parseError err 
     Right e  -> return e
 
-printApplication :: A.Expr -> [A.Expr] 
-printApplication = undefined
+-- * unparsing
+
+class Unparse c a where
+  unparse :: Scope m => a -> m c 
+
+instance Unparse c a => Unparse (Maybe c) (Maybe a) where
+  unparse = mapM unparse
+
+instance Unparse C.Declaration A.Declaration where
+  unparse adecl =
+    case adecl of
+      A.TypeSig n t -> C.TypeSig <$> getName n <*> unparse t
+      A.Defn n mt e -> C.Defn <$> getName n <*> unparse mt <*> unparse e
+
+instance Unparse C.Expr A.Expr where
+  unparse aexpr = 
+    case aexpr of
+      A.Atom a            -> C.Atom <$> unparse a
+      A.Pi Nothing  t1 t2 -> C.Fun <$> unparse t1 <*> unparse t2
+      A.Pi (Just x) t1 t2 -> C.Pi <$> getName x <*> unparse t1 <*> unparse t2
+      A.Lam x mt e        -> C.Lam <$> getName x <*> unparse mt <*> unparse e
+      A.App{}             -> C.Apps <$> mapM unparse (unparseApplication aexpr)
+
+instance Unparse C.Atom A.Atom where
+
+unparseApplication :: A.Expr -> [A.Expr] 
+unparseApplication = undefined
