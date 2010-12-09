962c9c77774162919481113558cb9271505af3cd
Andreas Abel <andreas.abel@ifi.lmu.de>
2010-12-09 14:47:35 +0000

    updated TypeCheck

---
+++ src/ScopeMonad.hs
@@ -0,0 +1,149 @@
+{- | An instance of the Scope monad constructed from monad transformers -}
+
+{-# LANGUAGE UndecidableInstances #-}
+
+module ScopeMonad where
+
+import Control.Applicative
+import Control.Monad.Error  hiding (mapM)
+import Control.Monad.Reader hiding (mapM)
+import Control.Monad.State  hiding (mapM)
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import qualified Concrete as C
+import qualified Abstract as A
+
+import Scoping
+{-
+import Scoping hiding (ParseError)
+import qualified Scoping
+type ParseError = Scoping.ParseError
+-}
+
+-- * implementation of scoping monad by reader/state/error
+
+{- Scoping need the following components
+
+- a map from C.Name to A.Name
+- a map from A.Name to (A.Ident, C.Fixity)
+
+  this resolves the parsed concrete names into unique identifiers
+  with their fixity 
+
+  My view: A fixity can only be set before the name is first used.
+  Twelf  : A fixity can be set and reset at any time.
+           Thus, a fixity is attached to the concrete name and not 
+           the abstract name.
+
+  I assume the set of fixity declarations to be small, thus, it does
+  not matter if there are separate lookups for fixity and abstract name.
+
+- a map from A.Name to C.Name
+
+-}
+
+data ScopeState = ScopeState
+  { counter   :: A.Name
+  , fixities  :: Map C.Name C.Fixity  -- assuming there are few fix.decls.
+  , renaming  :: Map C.Name A.Ident
+  , naming    :: Map A.Name C.Name    -- name suggestion
+  }
+
+initScopeState = ScopeState
+  { counter = 0
+  , fixities = Map.empty
+  , renaming = Map.empty
+  , naming   = Map.empty
+  }
+
+{-
+data ScopeEntry = ScopeEntry
+  { ident   :: A.Ident -- ^ to increase sharing, keep the whole ident here
+  , fixity :: C.Fixity
+  , name   :: C.Name -- ^ needs to be changed if shadowed 
+  }
+-}
+
+data ScopeContext = ScopeContext
+  { localRen :: Map C.Name A.Ident
+--  , localNam :: Map A.Name C.Name
+  }
+
+initScopeContext = ScopeContext
+  { localRen = Map.empty
+--  , localNam = Map.empty
+  }
+
+instance ( Applicative m
+         , MonadReader ScopeContext m
+         , MonadState  ScopeState   m
+         , MonadError  ParseError   m ) => Scope m where
+
+  addGlobal ident n = do
+    st@ScopeState { counter = x, renaming = ren, naming = nam } <- get
+    let mx   = Map.lookup n ren -- TODO: shadowing!!
+        ren' = Map.insert n (ident x) ren
+        nam' = Map.insert x n nam -- TODO: shadowing!
+    put $ st { counter = (x + 1), renaming = ren', naming = nam' }
+    return x
+{-
+  addCon n = do
+    ScopeState { counter = x, renaming = ren, naming = nam } <- get
+    let mx   = Map.lookup n ren -- TODO: shadowing!!
+        ren' = Map.insert n x ren
+        it   = ScopeEntry 
+                { ident = A.Con x 
+                , fixity = O.Nofix
+                , name = n -- TODO: shadowing!
+                }
+        nam' = Map.insert x it nam
+    put $ ScopeState (x + 1) ren' nam'
+    return x
+-} 
+
+  addFixity n fx = modify $ \ st -> st { fixities = Map.insert n fx (fixities st) } 
+
+  addVar n cont = do
+    st <- get
+    let x = counter st
+    put $ st { counter = x + 1 , naming = Map.insert x n (naming st) }
+    local (\ cxt -> cxt 
+      { localRen = Map.insert n (A.Var x) (localRen cxt)
+--      , localNam = Map.insert x n (localNam cxt) -- TODO: shadowing!
+      }) $ cont x
+
+{-
+  addVar n cont = do
+    st <- get
+    let x = counter st
+    put $ st { counter = x + 1 }
+    local (\ cxt -> cxt 
+      { localRen = Map.insert n (A.Var x) (localRen cxt)
+      , localNam = Map.insert x n (localNam cxt) -- TODO: shadowing!
+      }) $ cont x
+-}
+
+  getName x = do
+{-
+    nam <- asks localNam
+    case Map.lookup x nam of
+      Just n -> return n
+      Nothing -> do
+-}
+        nam <- gets naming
+        case Map.lookup x nam of
+          Just n -> return n
+          Nothing -> fail $ "error unbound abstract identifier " ++ show x
+
+  getFixity n = Map.lookup n <$> gets fixities
+
+  getIdent n = do
+    ren <- asks localRen
+    (\ cont -> maybe cont return $ Map.lookup n ren) $ do
+    ren <- gets renaming
+    (\ cont -> maybe cont return $ Map.lookup n ren) $ do
+    fail $ "error unbound concrete identifier " ++ show n
+
+  parseError = throwError
---
+++ src/Value.hs
@@ -0,0 +1,36 @@
+module Value where
+
+import Abstract
+
+data Sort 
+  = Type
+  | Kind -- only internally
+    deriving (Eq, Ord, Show)
+
+data TyView val
+  = VPi val val
+  | VSort Sort
+  | VBase 
+
+data TmView fvar val
+  = VNe  fvar val [val]  -- x^A vs
+  | VVal                 -- Abs, Pi, Type, ...
+
+class Eq fvar => Value fvar val | val -> fvar where
+  typ     :: val
+  kind    :: val
+  freeVar :: fvar -> val -> val      -- typed free variable
+  tyView  :: val -> TyView val
+  tmView  :: val -> TmView fvar val
+
+-- | The purpose of @MonadEval@ is read-only access to a global signature
+--   to get the definition of symbols, and potentially IO to have references
+--   to implement call-by-need.
+class MonadEval val env m | m -> val, m -> env where
+  apply     :: val  -> val -> m val
+  evaluate  :: Expr -> env -> m val  
+  evaluate' :: Expr -> m val        -- ^ evaluate a closed expression
+--  reify   :: val -> m Expr   -- this will need the local context
+
+
+  
